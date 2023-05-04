be5efa6b96f1e6481bbda51e601efce534aca8b3
RyvrImmersive <108792269+RyvrImmersive@users.noreply.github.com>
2023-05-04 09:34:59 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -18,20 +18,6 @@ def get_text():
 
 user_input = get_text()
 
-if user_input:
-    result = chain({"question": user_input})
-    output = f"Answer: {result['answer']}\nSources: {result['sources']}"
-
-    st.session_state.past.append(user_input)
-    st.session_state.generated.append(output)
-
-if st.session_state["generated"]:
-
-    for i in range(len(st.session_state["generated"]) - 1, -1, -1):
-        message(st.session_state["generated"][i], key=str(i))
-        message(st.session_state["past"][i], is_user=True, key=str(i) + "_user")
-
-
 from streamlit_chat import message
 import faiss
 from langchain import OpenAI
@@ -46,6 +32,35 @@ with open("faiss_store.pkl", "wb") as f:
 
 store.index = index
 chain = VectorDBQAWithSourcesChain.from_llm(llm=OpenAI(temperature=0), vectorstore=store)
+
+if user_input:
+    result = chain({"question": user_input})
+    output = f"Answer: {result['answer']}\nSources: {result['sources']}"
+
+    st.session_state.past.append(user_input)
+    st.session_state.generated.append(output)
+
+if st.session_state["generated"]:
+
+    for i in range(len(st.session_state["generated"]) - 1, -1, -1):
+        message(st.session_state["generated"][i], key=str(i))
+        message(st.session_state["past"][i], is_user=True, key=str(i) + "_user")
+
+
+# from streamlit_chat import message
+# import faiss
+# from langchain import OpenAI
+# from langchain.chains import VectorDBQAWithSourcesChain
+# import pickle
+
+# # Load the LangChain.
+# index = faiss.read_index("docs.index")
+
+# with open("faiss_store.pkl", "wb") as f:
+#     store = pickle.load(f)
+
+# store.index = index
+# chain = VectorDBQAWithSourcesChain.from_llm(llm=OpenAI(temperature=0), vectorstore=store)
 
 
 # # From here down is all the StreamLit UI.
