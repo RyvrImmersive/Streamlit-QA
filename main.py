6308ac51bfc4ce9cbf6de1ec93bf8a1a58a3e6b8
RyvrImmersive <108792269+RyvrImmersive@users.noreply.github.com>
2023-05-04 09:43:12 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ import pickle
 # Load the LangChain.
 index = faiss.read_index("docs.index")
 
-with open("faiss_store.pkl", "wb") as f:
+with open("faiss_store.pkl", "ab+") as f:
     store = pickle.load(f)
 
 store.index = index
